d78ff5e8ba42a4dd208ca6502672557067673814
moalshaaban <66526800+moalshaaban@users.noreply.github.com>
2020-06-20 18:38:29 +0300

    التمرين الاول

---
+++ ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+var number1 = 2
+var number2 = 4
+
+func add( a: Int, b: Int ) ->Int
+{
+    return a + b
+}
+var functionCall = add(a: number1, b: number2)
+print(" \(number1) + \(number2) = \(functionCall) ")
